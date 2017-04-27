671a119c6674eef91ee17ac81c2df6091eadde6a
Abdullah Abdulhaque <abdullah.abdulhaque@math.ntnu.no>
2017-04-27 08:20:57 +0200

    First Commit

---
+++ Settings.h
@@ -0,0 +1,10 @@
+// Including the standard C++ libraries.
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+// Including the relevant classes.
+#include "IGA_1D/IGA_1D.h"
+
+
+using namespace std;
